aad02d14ecaa84176419cee8da30416785246816
Yochannah Yehudi <yochannah@gmail.com>
2017-09-05 18:47:55 +0100

    my mock notifications now show up on the map. Next up: give it time series and filters.

--- js/main.js
+++ js/main.js
@@ -2,44 +2,57 @@ var partnerCode = "ATIDSG";
 //crossorigin.me gets around the fact that the api headers don't allow CORS requests
 // :'(
 var careUrl = "https://crossorigin.me/https://api.cqc.org.uk/public/v1/locations",
-postcodeUrl = "https://api.postcodes.io/postcodes/"
+    postcodeUrl = "https://api.postcodes.io/postcodes/"
 
 
 
-function Cqc(){
-  var parent = this;
-  this.getLocation = function (locationId) {
-    return $.ajax(careUrl + "/" + locationId + "?partnerCode=" + partnerCode, {'method':'get'});
-  };
-  this.getNotifications = function(details) {
-    //if there is an api for notifications,
-    //replace this method with the appropriate calls.
+function Cqc() {
+    var parent = this;
+    this.getLocation = function(locationId) {
+      console.log(locationId);
+        return $.ajax(careUrl + "/" + locationId + "?partnerCode=" + partnerCode, {
+            'method': 'get'
+        });
+    };
+    this.getNotifications = function(details) {
+        //if there is an api for notifications,
+        //replace this method with the appropriate calls.
 
-    return tinyMockData[details.year][details.month];
-    //this is for bigger-scale
-//    return mockNotifications[details.year][details.month];
-  };
-  this.lookupPostcode = function(postcode){
-    return $.ajax(postcodeUrl + postcode, {'method':'get'});
-  }
-  this.showNotifications = function (details) {
-    var notifications = getNotifications(details);
-    for (locationId in notifications) {
-      // fetch details from api
-      var location = parent.getLocation(locationId);
-      location.done(function(response){
-        var postcode = lookupPostcode(response.postalCode);
-        postcode.done(function(x) {
-          console.log(x.result.latitude,x.result.latitude);
-        })
-      })
-      // add marker to map
-      console.log(locationId, notifications[locationId]);
+        return tinyMockData[details.year][details.month];
+        //this is for bigger-scale
+        //    return mockNotifications[details.year][details.month];
+    };
+    this.lookupPostcode = function(postcode) {
+        return $.ajax(postcodeUrl + postcode, {
+            'method': 'get'
+        });
     }
-  }
-  return this;
+    this.showNotifications = function(details) {
+        var notifications = getNotifications(details);
+        notificationids = Object.keys(notifications);
+        for (var i = 0; i < notificationids.length; i++) {
+            // fetch details from api
+            loc = parent.getLocation(notificationids[i]);
+            loc.done(function(response) {
+                var postcode = lookupPostcode(response.postalCode);
+                postcode.done(function(x) {
+                    // add marker to map
+                    addMarker({
+                        locationId: response.locationId,
+                        notification: notifications[response.locationId],
+                        Latitude : x.result.latitude,
+                        Longitude : x.result.longitude
+                    } );
+                })
+            })
+        }
+    }
+    return this;
 }
 
 var cqc = Cqc();
 
-cqc.showNotifications({year: 2017, month : 01});
+cqc.showNotifications({
+    year: 2017,
+    month: 07
+});
---
+++ js/map.js
@@ -0,0 +1,66 @@
+var mymap = L.map('mapid').setView([53.382121, -1.467878], 1);
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoieW9jaGFubmFoIiwiYSI6Iko5TU1xcW8ifQ.AlR1faR7rfR1CoJRyIPEAg', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 6,
+    id: 'notsure',
+    accessToken: 'pk.eyJ1IjoieW9jaGFubmFoIiwiYSI6Iko5TU1xcW8ifQ.AlR1faR7rfR1CoJRyIPEAg'
+}).addTo(mymap);
+
+var markers = {},
+colors= {
+  alert : "#ff0000",
+  concern : "#ffff00"
+};
+
+function addMarker(marker, b) {
+//console.log(marker, markers);
+    //assume array of objs with lats and longs and other fun stuff
+    var lat = marker.Latitude,
+        long = marker.Longitude,
+        sev = marker.notification.notificationType,
+        bounds = mymap.getBounds(),
+        zoomLevel = mymap.getZoom();
+
+        //only add markers if they haven't already been added
+        //we don't want to waste resources adding markers we can't see anyway.
+        circle = L.circle([lat, long], {
+            fillColor: colors[sev],
+            color: colors[sev],
+            weight: 1,
+            opacity: 0.5,
+            fillOpacity: 0.33,
+            radius: (100000 / mymap.getZoom())
+        }).addTo(mymap);
+        markers[marker.locationId] = circle;
+}
+
+function removeMarker(marker, k) {
+    mymap.removeLayer(marker);
+    delete markers[k];
+}
+
+function removeAllMarkers() {
+    for (marker in markers) {
+        removeMarker(markers[marker], marker);
+    }
+}
+
+function getBounds() {
+    var b = mymap.getBounds();
+    return {
+        startAt: b._southWest.lat,
+        endAt: b._northEast.lat,
+        bucket: mymap.getCenter().lng.toString().split(".")[0]
+    }
+}
+
+function centerMap(lat, long, zoomin) {
+    mymap.panTo([lat, long]);
+    if (zoomin) {
+        mymap.setZoom(14);
+    } else {
+        mymap.setZoom(12);
+
+    }
+}
