af006d112c121344c606010580eca8bfa4e55ce1
Yochannah Yehudi <yochannah@gmail.com>
2017-09-05 13:38:58 +0100

    generate list of locations in cambridgeshire for sample data

---
+++ js/generate-random-notification.js
@@ -0,0 +1,75 @@
+const csvFilePath = 'data/23_August_2017_CQC_directory.csv';
+
+var fieldDefinitions = {
+    prettyNames: [ //currently prettynames isn't used
+        "Name",
+        "Also known as",
+        "Address",
+        "Postcode",
+        "Phone number",
+        "Service's website (if available)",
+        "Service types",
+        "Date of latest check",
+        "Specialisms/services",
+        "Provider name",
+        "Local Authority",
+        "Region",
+        "Location URL",
+        "CQC Location (for office use only",
+        "CQC Provider ID (for office use only)"
+    ],
+    computerNames: [
+        "name",
+        "aka",
+        "address",
+        "postcode",
+        "phone",
+        "website",
+        "types",
+        "latestCheck",
+        "services",
+        "provider",
+        "localAuthority",
+        "region",
+        "locationURL",
+        "locationId",
+        "providerId"
+    ],
+
+    ignore: [
+        "aka",
+        "address",
+        "phone",
+        "website",
+        "locationURL",
+        "providerId"
+    ]
+};
+
+
+
+/** hopefully you won't need to edit anything below here **/
+const csv = require('csvtojson');
+
+var results = [],
+    types = [],
+    services = [];
+
+
+csv({
+        headers: fieldDefinitions.computerNames,
+        includeColumns: ["locationId","localAuthority"],
+
+    })
+    .fromFile(csvFilePath)
+    .on('json', (jsonObj, index) => {
+        // combine csv header row and csv line to a json object
+        // jsonObj.a ==> 1 or 4
+        if (jsonObj.localAuthority == "Cambridgeshire") {
+        results.push (jsonObj.locationId);
+      }
+    })
+    .on('done', (error) => {
+        //    console.log('end')
+        console.log(results);
+    })
