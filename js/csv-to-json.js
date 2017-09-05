ba6fadee8253bc8adbbc2daf1b85af2267f2fc85
Yochannah Yehudi <yochannah@gmail.com>
2017-09-05 11:07:08 +0100

    basic csv-json conversion + remove irrelevant columns. File size still really too big though

---
+++ js/csv-to-json.js
@@ -0,0 +1,68 @@
+var fieldDefinitions = {};
+fieldDefinitions.prettyNames = [
+    "Name",
+    "Also known as",
+    "Address",
+    "Postcode",
+    "Phone number",
+    "Service's website (if available)",
+    "Service types",
+    "Date of latest check",
+    "Specialisms/services",
+    "Provider name",
+    "Local Authority",
+    "Region",
+    "Location URL",
+    "CQC Location (for office use only",
+    "CQC Provider ID (for office use only)"
+]
+
+fieldDefinitions.computerNames = [
+    "name",
+    "aka",
+    "address",
+    "postcode",
+    "phone",
+    "website",
+    "types",
+    "latestCheck",
+    "services",
+    "provider",
+    "localAuthority",
+    "region",
+    "locationURL",
+    "locationId",
+    "providerId"
+]
+
+fieldDefinitions.ignore = [
+    "aka",
+    "address",
+    "phone",
+    "website",
+    "locationURL",
+    "providerId"
+];
+
+const csvFilePath = 'data/23_August_2017_CQC_directory.csv'
+const csv = require('csvtojson');
+
+var results = {},
+ types = [],
+ services = [];
+
+
+csv({
+        headers: fieldDefinitions.computerNames,
+        ignoreColumns: fieldDefinitions.ignore
+    })
+    .fromFile(csvFilePath)
+    .on('json', (jsonObj, index) => {
+        // combine csv header row and csv line to a json object
+        // jsonObj.a ==> 1 or 4
+        results[jsonObj.locationId] = jsonObj;
+    })
+    .on('done', (error) => {
+        //    console.log('end')
+        console.log(results);
+    })
