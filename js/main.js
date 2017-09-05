bd250e3fabdaed3e0cd66ef4de70221fb88ed486
Yochannah Yehudi <yochannah@gmail.com>
2017-09-05 12:50:30 +0100

    added some mocked data and GET from API. Needs better mock data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 var partnerCode = "ATIDSG";
+var url = "https://api.cqc.org.uk/public/v1/locations"
+
+
+
+function Cqc(){
+  var parent = this;
+  this.getLocation = function (locationId) {
+    return $.ajax(url + "/" + locationId)
+    .done(function (response) {
+      console.log(response);
+    })
+  };
+  this.getNotifications = function(details) {
+    //if there is an api for notifications,
+    //replace this method with the appropriate calls.
+
+    return mockNotifications[details.year][details.month];
+  };
+  this.showNotifications = function (details) {
+    var notifications = getNotifications(details);
+    for (locationId in notifications) {
+      // fetch details from api
+      var location = parent.getLocation(locationId);
+      // add marker to map
+      console.log(locationId, notifications[locationId]);
+    }
+  }
+  return this;
+}
+
+var cqc = Cqc();
+
+cqc.showNotifications({year: 2016, month : 01});
---
+++ js/mockdata.js
@@ -0,0 +1,59 @@
+var mockNotifications = {
+    2016: {
+        01: {
+            "1-1000210669": {
+                notificationType: "alert",
+                details: "Details of notification here",
+            },
+            "1-1000210671": {
+                notificationType: "concern",
+                details: "Details of notification here",
+            },
+            "1-1000210672": {
+                notificationType: "concern",
+                details: "Details of notification here",
+            },
+            "1-1000210673": {
+                notificationType: "alert",
+                details: "Details of notification here",
+            },
+            "1-1000210674": {
+                notificationType: "concern",
+                details: "Details of notification here",
+            }
+        },
+        02 :         {
+                    "1-1000210669": {
+                        notificationType: "alert",
+                        details: "Details of notification here",
+                    },
+                    "1-1000210671": {
+                        notificationType: "concern",
+                        details: "Details of notification here",
+                    },
+                    "1-1000210672": {
+                        notificationType: "concern",
+                        details: "Details of notification here",
+                    },
+                    "1-1000210673": {
+                        notificationType: "alert",
+                        details: "Details of notification here",
+                    },
+                    "1-1000210674": {
+                        notificationType: "concern",
+                        details: "Details of notification here",
+                    },
+                    "1-1000210672": {
+                        notificationType: "alert",
+                        details: "Details of notification here",
+                    },
+                    "1-1000210673": {
+                        notificationType: "concern",
+                        details: "Details of notification here",
+                    },
+                    "1-1000210674": {
+                        notificationType: "concern",
+                        details: "Details of notification here",
+                    }}
+    }
+}
