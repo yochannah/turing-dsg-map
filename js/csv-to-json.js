ce9a24e92244c678ea447d422d7f7ee3d95a24dd
Yochannah Yehudi <yochannah@gmail.com>
2017-09-05 11:18:58 +0100

    minor refactor for beauty

--- js/csv-to-json.js
+++ js/csv-to-json.js
@@ -1,57 +1,57 @@
 const csvFilePath = 'data/23_August_2017_CQC_directory.csv';
 
-var fieldDefinitions = {};
-fieldDefinitions.prettyNames = [
-    "Name",
-    "Also known as",
-    "Address",
-    "Postcode",
-    "Phone number",
-    "Service's website (if available)",
-    "Service types",
-    "Date of latest check",
-    "Specialisms/services",
-    "Provider name",
-    "Local Authority",
-    "Region",
-    "Location URL",
-    "CQC Location (for office use only",
-    "CQC Provider ID (for office use only)"
-]
+var fieldDefinitions = {
+    prettyNames: [ //currently prettynames isn't used
+        "Name",
+        "Also known as",
+        "Address",
+        "Postcode",
+        "Phone number",
+        "Service's website (if available)",
+        "Service types",
+        "Date of latest check",
+        "Specialisms/services",
+        "Provider name",
+        "Local Authority",
+        "Region",
+        "Location URL",
+        "CQC Location (for office use only",
+        "CQC Provider ID (for office use only)"
+    ],
+    computerNames: [
+        "name",
+        "aka",
+        "address",
+        "postcode",
+        "phone",
+        "website",
+        "types",
+        "latestCheck",
+        "services",
+        "provider",
+        "localAuthority",
+        "region",
+        "locationURL",
+        "locationId",
+        "providerId"
+    ],
 
-fieldDefinitions.computerNames = [
-    "name",
-    "aka",
-    "address",
-    "postcode",
-    "phone",
-    "website",
-    "types",
-    "latestCheck",
-    "services",
-    "provider",
-    "localAuthority",
-    "region",
-    "locationURL",
-    "locationId",
-    "providerId"
-]
-
-fieldDefinitions.ignore = [
-    "aka",
-    "address",
-    "phone",
-    "website",
-    "locationURL",
-    "providerId"
-];
+    ignore: [
+        "aka",
+        "address",
+        "phone",
+        "website",
+        "locationURL",
+        "providerId"
+    ]
+};
 
 /** hopefully you won't need to edit anything below here **/
 const csv = require('csvtojson');
 
 var results = {},
- types = [],
- services = [];
+    types = [],
+    services = [];
 
 
 csv({
