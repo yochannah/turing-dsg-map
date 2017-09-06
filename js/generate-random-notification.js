2900baad6706958f89431294f79ed3467306099c
Yochannah Yehudi <yochannah@gmail.com>
2017-09-06 15:00:27 +0100

    regenerate fake data based on a batter understanding of the data structure

--- js/generate-random-notification.js
+++ js/generate-random-notification.js
@@ -1,49 +1,49 @@
 const csvFilePath = 'data/23_August_2017_CQC_directory.csv';
 
 var fieldDefinitions = {
-    prettyNames: [ //currently prettynames isn't used
-        "Name",
-        "Also known as",
-        "Address",
-        "Postcode",
-        "Phone number",
-        "Service's website (if available)",
-        "Service types",
-        "Date of latest check",
-        "Specialisms/services",
-        "Provider name",
-        "Local Authority",
-        "Region",
-        "Location URL",
-        "CQC Location (for office use only",
-        "CQC Provider ID (for office use only)"
-    ],
-    computerNames: [
-        "name",
-        "aka",
-        "address",
-        "postcode",
-        "phone",
-        "website",
-        "types",
-        "latestCheck",
-        "services",
-        "provider",
-        "localAuthority",
-        "region",
-        "locationURL",
-        "locationId",
-        "providerId"
-    ],
+  prettyNames: [ //currently prettynames isn't used
+    "Name",
+    "Also known as",
+    "Address",
+    "Postcode",
+    "Phone number",
+    "Service's website (if available)",
+    "Service types",
+    "Date of latest check",
+    "Specialisms/services",
+    "Provider name",
+    "Local Authority",
+    "Region",
+    "Location URL",
+    "CQC Location (for office use only",
+    "CQC Provider ID (for office use only)"
+  ],
+  computerNames: [
+    "name",
+    "aka",
+    "address",
+    "postcode",
+    "phone",
+    "website",
+    "types",
+    "latestCheck",
+    "services",
+    "provider",
+    "localAuthority",
+    "region",
+    "locationURL",
+    "locationId",
+    "providerId"
+  ],
 
-    ignore: [
-        "aka",
-        "address",
-        "phone",
-        "website",
-        "locationURL",
-        "providerId"
-    ]
+  ignore: [
+    "aka",
+    "address",
+    "phone",
+    "website",
+    "locationURL",
+    "providerId"
+  ]
 };
 
 
@@ -52,65 +52,74 @@ var fieldDefinitions = {
 const csv = require('csvtojson');
 
 var results = [],
-    types = [],
-    services = [];
+  types = [],
+  services = [];
 
 
 csv({
-        headers: fieldDefinitions.computerNames,
-        includeColumns: ["locationId", "localAuthority"],
+    headers: fieldDefinitions.computerNames,
+    includeColumns: ["locationId", "localAuthority"],
 
-    })
-    .fromFile(csvFilePath)
-    .on('json', (jsonObj, index) => {
-        // combine csv header row and csv line to a json object
-        // jsonObj.a ==> 1 or 4
-            results.push(jsonObj.locationId);
-    })
-    .on('done', (error) => {
-        //    console.log('end')
-        //console.log(results);
-        //fakeMonth([2016])
-        tinyMockData([2017])
-    });
-var concernOrAlert = function() {
-    return (Math.random() > 0.5) ? "concern" : "alert";
+  })
+  .fromFile(csvFilePath)
+  .on('json', (jsonObj, index) => {
+    // combine csv header row and csv line to a json object
+    // jsonObj.a ==> 1 or 4
+    results.push(jsonObj.locationId);
+  })
+  .on('done', (error) => {
+    //    console.log('end')
+    //console.log(results);
+    //fakeMonth([2016])
+    fakeData([2016, 2017])
+  });
+
+var notificationTypes = ["DOLs",
+  "Abuse",
+  "Injury",
+  "Whistleblower",
+  "Safeguarding"
+];
+var notificationType = function() {
+  return notificationTypes[Math.round(Math.random() * 5)];
 }
 
-var fakeMonth = function(years) {
-    var response = {},
-    numOfResults = results.length;
-    years.map(function(year) {
-        response[year] = {};
-        for (var month = 1; month <= 12; month++) {
-            response[year][month] = {};
-            for (var i = 1; i <= 1000; i++) {
-                var id = Math.floor(Math.random() * numOfResults),
-                    locationId = results[id];
-                response[year][month][locationId] = {
-                    "notificationType": concernOrAlert()
-                };
-            }
-        }
-    });
-    console.log(JSON.stringify(response));
+function randomIntFromInterval(min, max) {
+  //thanks, SO: https://stackoverflow.com/questions/4959975xw
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var tinyMockData = function(years) {
-    var response = {},
+var fakeCount = function() {
+  var highOrLow = Math.random();
+  if (highOrLow > 0.9) {
+    return randomIntFromInterval(3, 5);
+  } else {
+    if (highOrLow > 0.7) {
+      return 2
+    } else {
+      return 1;
+    }
+  }
+};
+
+var fakeData = function(years) {
+  var response = {},
     numOfResults = results.length;
-    years.map(function(year) {
-        response[year] = {};
-        for (var month = 1; month <= 12; month++) {
-            response[year][month] = {};
-            for (var i = 1; i <= 10; i++) {
-                var id = Math.floor(Math.random() * numOfResults),
-                    locationId = results[id];
-                response[year][month][locationId] = {
-                    "notificationType": concernOrAlert()
-                };
-            }
-        }
-    });
-    console.log(JSON.stringify(response));
+  years.map(function(year) {
+    response[year] = {};
+    console.log(notificationType());
+    //js dates are 0 based, so 6 is nooootttt June, it's July.
+    for (var month = 0; month <= 11; month++) {
+      response[year][month] = {};
+      for (var i = 1; i <= 10; i++) {
+        var id = Math.floor(Math.random() * numOfResults),
+          locationId = results[id];
+        response[year][month][locationId] = {
+          "notificationType": notificationType(),
+          "number": fakeCount()
+        };
+      }
+    }
+  });
+  console.log(JSON.stringify(response));
 }
--- js/map.js
+++ js/map.js
@@ -1,66 +1,70 @@
 var mymap = L.map('mapid').setView([53.382121, -1.467878], 1);
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoieW9jaGFubmFoIiwiYSI6Iko5TU1xcW8ifQ.AlR1faR7rfR1CoJRyIPEAg', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    minZoom: 6,
-    id: 'notsure',
-    accessToken: 'pk.eyJ1IjoieW9jaGFubmFoIiwiYSI6Iko5TU1xcW8ifQ.AlR1faR7rfR1CoJRyIPEAg'
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  minZoom: 6,
+  id: 'notsure',
+  accessToken: 'pk.eyJ1IjoieW9jaGFubmFoIiwiYSI6Iko5TU1xcW8ifQ.AlR1faR7rfR1CoJRyIPEAg'
 }).addTo(mymap);
 
 var markers = {},
-colors= {
-  alert : "#ff0000",
-  concern : "#ffff00"
-};
+  colors = {
+    "DOLs" : "#8BC34A",
+    "Abuse":"#2196F3",
+    "Injury":"#E91E63",
+    "Whistleblower":"#FFC107",
+    "Safeguarding":"#673AB7"
+
+  };
 
 function addMarker(marker, b) {
-//console.log(marker, markers);
-    //assume array of objs with lats and longs and other fun stuff
-    var lat = marker.latitude,
-        long = marker.longitude,
-        sev = marker.notification.notificationType,
-        bounds = mymap.getBounds(),
-        zoomLevel = mymap.getZoom();
+  //console.log(marker, markers);
+  //assume array of objs with lats and longs and other fun stuff
+  var lat = marker.latitude,
+    long = marker.longitude,
+    sev = marker.notification.notificationType,
+    bounds = mymap.getBounds(),
+    zoomLevel = mymap.getZoom();
 
-        //only add markers if they haven't already been added
-        //we don't want to waste resources adding markers we can't see anyway.
-        circle = L.circle([lat, long], {
-            fillColor: colors[sev],
-            color: colors[sev],
-            weight: 1,
-            opacity: 0.5,
-            fillOpacity: 0.33,
-            radius: (100000 / mymap.getZoom())
-        }).addTo(mymap);
-        markers[marker.locationId] = circle;
+  //only add markers if they haven't already been added
+  //we don't want to waste resources adding markers we can't see anyway.
+  circle = L.circle([lat, long], {
+    fillColor: colors[sev],
+    color: colors[sev],
+    weight: 1,
+    opacity: marker.notification.number/10,
+    fillOpacity: marker.notification.number/10 + 0.3,
+    radius: (100000 / mymap.getZoom())
+  }).addTo(mymap);
+  markers[marker.locationId] = circle;
 }
 
 function removeMarker(marker, k) {
-    mymap.removeLayer(marker);
-    delete markers[k];
+  mymap.removeLayer(marker);
+  delete markers[k];
 }
 
 function removeAllMarkers() {
-    for (marker in markers) {
-        removeMarker(markers[marker], marker);
-    }
+  for (marker in markers) {
+    removeMarker(markers[marker], marker);
+  }
 }
 
 function getBounds() {
-    var b = mymap.getBounds();
-    return {
-        startAt: b._southWest.lat,
-        endAt: b._northEast.lat,
-        bucket: mymap.getCenter().lng.toString().split(".")[0]
-    }
+  var b = mymap.getBounds();
+  return {
+    startAt: b._southWest.lat,
+    endAt: b._northEast.lat,
+    bucket: mymap.getCenter().lng.toString().split(".")[0]
+  }
 }
 
 function centerMap(lat, long, zoomin) {
-    mymap.panTo([lat, long]);
-    if (zoomin) {
-        mymap.setZoom(14);
-    } else {
-        mymap.setZoom(12);
+  mymap.panTo([lat, long]);
+  if (zoomin) {
+    mymap.setZoom(14);
+  } else {
+    mymap.setZoom(12);
 
-    }
+  }
 }
